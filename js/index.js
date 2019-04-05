bbf4ac45c786e22eebee5ebbbc86704e5db30bfa
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-04-05 17:13:19 -0500

    comment out jquery

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
 
-function saveLincoln(interval) {
-  if(isNaN(interval)) {
-    interval = 10000
-  }
-  $('#foreground').fadeOut(interval);
-}
+// $('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+//
+// function saveLincoln(interval) {
+//   if(isNaN(interval)) {
+//     interval = 10000
+//   }
+//   $('#foreground').fadeOut(interval);
+// }
